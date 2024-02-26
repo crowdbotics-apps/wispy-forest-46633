b4c9788963fd9d040afd43cde6a4f23aae068c7c
Crowdbotics <team@crowdbotics.com>
2024-02-26 05:40:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wispy-forest-46633",
+  url: "https://wispy-forest-46633.botics.co",
+  api: "https://wispy-forest-46633.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
